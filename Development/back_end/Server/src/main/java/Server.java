27d5fedee626759df0cdc99d1da998d0947d9cfa
Yunyi Gao <gao622@purdue.edu>
2024-11-02 13:05:54 -0400

    Adjust code structure, added comments.

--- Development/back_end/Server/src/main/java/Server.java
+++ Development/back_end/Server/src/main/java/Server.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
@@ -20,18 +17,96 @@ public class Server {
 
     // Fields
     public int stage;
-    private static CopyOnWriteArrayList<ClientHandler> hosts = new CopyOnWriteArrayList<>();
+    private static CopyOnWriteArrayList<ClientHandler> clients = new CopyOnWriteArrayList<>();
 
     // Constructor
     Server() {
         this.stage = 1;
     }
 
-    // Classroom thread runnable
-    static class ClientHandler extends Thread {
 
-        DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
-        DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
+    /**
+     * @param client
+     * @param request
+     * @throws IOException
+     * Receives HTTP requests and send back responds.
+     * !!This methods should be used OUTSIDE of classroom
+     * TODO Add a HTTPRequest class to break down requests
+     */
+    public static void handleHTTPRequest(Socket client, StringBuilder request) throws IOException {
+        // Print request
+        System.out.println("--REQUEST--");
+        System.out.println(request);
+
+        // Start a classroom
+        String[] requestText = request.toString().split(" ");
+        if (requestText[1].equals("/Classroom")) {
+            // ?Keep this?
+        } else {
+            // Respond to request
+            OutputStream clientOut = client.getOutputStream();
+            clientOut.write("HTTP/1.1 200 OK\n".getBytes(StandardCharsets.UTF_8));
+            clientOut.write("\n".getBytes(StandardCharsets.UTF_8));
+            clientOut.write("Hi Nunu!\n".getBytes(StandardCharsets.UTF_8));
+            clientOut.flush();
+        }
+    }
+
+    /**
+     * @param message
+     * Broadcast received message to all connected clients.
+     */
+    public static void broadcast(String message) {
+        for (ClientHandler client : clients) {
+            try {
+                client.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    /**
+     * @throws Exception
+     * Entry method of Server.
+     * Start the server, called by main().
+     * Forever wait to accept connections from clients, and create a thread for each client.
+     */
+    public void startServer() throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server started");       // Start Message
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.toString());       // Client connected
+
+                // Input organize
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+                Thread clientHandler = new ClientHandler(clientSocket, in, out);
+                clients.add((ClientHandler) clientHandler);
+                clientHandler.start();
+
+            } catch (IOException e) {
+                serverSocket.close();           //! May cause server failure
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    /**
+     * Client Handler, called by startServer().
+     * Handle client connections under TCP connection (java Socket).
+     * Current: longterm TCP connection, send back the same message as the client sends it.
+     * ?Receive HTTP request, if request is to start or enter a classroom, keep the connection
+     */
+    static class ClientHandler extends Thread {
 
         private Socket clientSocket;
         private DataInputStream in;
@@ -52,11 +127,10 @@ public class Server {
         public void run() {
             String received;
             String toreturn;
-            while (true)
-            {
+            while (true) {
                 try {
 
-                    // Ask user what he wants
+                    // Send message to Client, asking what they want.
                     out.writeUTF("What do you want to say?\n"+
                             "Type EXIT to terminate connection.");
 
@@ -72,12 +146,13 @@ public class Server {
                         break;
                     }
 
-                    // Write back
+                    // Respond
                     out.writeUTF("You said: " + received);
                     Date date = new Date();
 
                 } catch (SocketException e) {
                     System.out.println("Unexpected disconnection from client: " + this.clientSocket);
+                    clients.remove(this);
                     break;
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -89,89 +164,13 @@ public class Server {
                 // closing resources
                 this.in.close();
                 this.out.close();
+                this.clientSocket.close();
 
-            }catch(IOException e){
+            } catch(IOException e){
                 e.printStackTrace();
             }
         }
     }
-
-    public static void handleHTTPRequest(Socket client, StringBuilder request) throws IOException {
-        // Print request
-        System.out.println("--REQUEST--");
-        System.out.println(request);
-
-        // Start a classroom
-        String[] requestText = request.toString().split(" ");
-        if (requestText[1].equals("/Classroom")) {
-            // TODO Pull up the class user list
-//            ClientHandler host = new ClientHandler(client);
-//            hosts.add(host);
-//            new Thread(host).start();
-        } else {
-            // Respond to request
-            OutputStream clientOut = client.getOutputStream();
-            clientOut.write("HTTP/1.1 200 OK\n".getBytes(StandardCharsets.UTF_8));
-            clientOut.write("\n".getBytes(StandardCharsets.UTF_8));
-            clientOut.write("Hi Nunu!\n".getBytes(StandardCharsets.UTF_8));
-            clientOut.flush();
-        }
-    }
-
-    // Broadcast received message to all clients
-    public static void broadcast(String message) {
-        for (ClientHandler host : hosts) {
-            try {
-                host.sendMessage(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-
-    /**
-     *
-     * // Classroom thread entry
-     * public int hostClassroom(ScienceClass scienceClass) {
-     *         Thread thread = new Thread(() -> {
-     *             System.out.println("Starting Classroom");
-     *         });
-     *         thread.start();
-     *
-     *         return 0;
-     *     }
-     *
-     */
-
-
-    // Start the server
-    public void startServer() throws Exception {
-
-        ServerSocket serverSocket = new ServerSocket(8080);
-        System.out.println("Server started");       // Start Message
-        while (true) {
-            try {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected: " + clientSocket.toString());       // Client connected
-
-                // Input organize
-                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-
-                Thread clientHandler = new ClientHandler(clientSocket, in, out);
-                hosts.add((ClientHandler) clientHandler);
-                clientHandler.start();
-
-            } catch (IOException e) {
-                serverSocket.close();
-                e.printStackTrace();
-            }
-        }
-
-    }
-
 
     public static void main (String[] args) {
 
